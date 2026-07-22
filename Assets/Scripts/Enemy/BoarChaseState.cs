ae9bd34203952db261953fff471370f9c62568ca
chongmuchen <chongmu.chen@gmail.com>
2026-07-22 12:26:45 +0800

    init

--- Assets/Scripts/Enemy/BoarChaseState.cs
+++ Assets/Scripts/Enemy/BoarChaseState.cs
@@ -6,10 +6,19 @@ public class BoarChaseSate : BaseState
     {
         _currentEnemy = enemy;
         Debug.Log("Boar Chase");
+        _currentEnemy.currentSpeed = _currentEnemy.chaseSpeed;
+        _currentEnemy.anim.SetBool("run", true);
     }
 
     public override void LogicUpdate()
     {
+        bool touchFrontWall = _currentEnemy.faceDir.x < 0
+            ? _currentEnemy.physics.touchLeftWall
+            : _currentEnemy.physics.touchRightWall;
+        if (!_currentEnemy.physics.isGround || touchFrontWall)
+        {
+            _currentEnemy.transform.localScale = new Vector3(_currentEnemy.faceDir.x, 1, 1);
+        }
     }
 
     public override void PhysicsUpdate()
