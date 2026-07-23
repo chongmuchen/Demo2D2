8353b313cb1ec4f068f9709c1330275ebae80bf1
chongmuchen <chongmu.chen@gmail.com>
2026-07-23 12:41:59 +0800

    slide

--- Assets/Scripts/General/PhysicsCheck.cs
+++ Assets/Scripts/General/PhysicsCheck.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 public class PhysicsCheck : MonoBehaviour
 {
     private CapsuleCollider2D _collider;
+    private PlayerController _playerController;
     public LayerMask groundLayer;
     [Header("参数检测")] public bool manual;
+    public bool isPlayer;
     public float checkRaduis;
 
     public Vector2 bottomOffset;
     public Vector2 frontOffset;
+    public Vector2 backOffset;
 
     [Header("状态")] public bool isGround;
+    public bool onWall;
     public bool touchFrontWall;
+    public bool touchBackWall;
 
     private void Awake()
     {
@@ -20,6 +25,12 @@ public class PhysicsCheck : MonoBehaviour
         if (!manual)
         {
             frontOffset = new Vector2(-(_collider.bounds.size.x + _collider.offset.x) / 2, _collider.bounds.size.y / 2);
+            backOffset = new Vector2(-frontOffset.x, backOffset.y);
+        }
+
+        if (isPlayer)
+        {
+            _playerController = GetComponent<PlayerController>();
         }
     }
 
@@ -33,12 +44,20 @@ public class PhysicsCheck : MonoBehaviour
     {
         isGround = Physics2D.OverlapCircle(GetPosByOffset(bottomOffset), checkRaduis, groundLayer);
         touchFrontWall = Physics2D.OverlapCircle(GetPosByOffset(frontOffset), checkRaduis, groundLayer);
+        touchBackWall = Physics2D.OverlapCircle(GetPosByOffset(backOffset), checkRaduis, groundLayer);
+        if (isPlayer)
+        {
+            var forward = (_playerController.inputDirection.x > 0 && transform.localScale.x > 0) ||
+                          (_playerController.inputDirection.x < 0 && transform.localScale.x < 0);
+            onWall = (touchFrontWall || touchBackWall) && !isGround && forward;
+        }
     }
 
     void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(GetPosByOffset(bottomOffset), checkRaduis);
         Gizmos.DrawWireSphere(GetPosByOffset(frontOffset), checkRaduis);
+        Gizmos.DrawWireSphere(GetPosByOffset(backOffset), checkRaduis);
     }
 
     public Vector2 GetPosByOffset(Vector2 offset)
