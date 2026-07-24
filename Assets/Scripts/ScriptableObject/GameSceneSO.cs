6af332ddcb97b9157bb3084424a6c21ed0bc5121
chongmuchen <chongmu.chen@gmail.com>
2026-07-24 17:20:15 +0800

    切场景

--- Assets/Scripts/ScriptableObject/GameSceneSO.cs
+++ Assets/Scripts/ScriptableObject/GameSceneSO.cs
@@ -5,5 +5,6 @@ using UnityEngine.AddressableAssets;
 [CreateAssetMenu(fileName = "GameSceneSO", menuName = "Game Scene/GameSceneSO")]
 public class GameSceneSO : ScriptableObject
 {
+    public SceneType sceneType;
     public AssetReference sceneReference;
 }
--- Assets/Scripts/Transition/SceneLoader.cs
+++ Assets/Scripts/Transition/SceneLoader.cs
@@ -1,16 +1,66 @@
+using System;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using System.Collections;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("事件监听")] public SceneLoadEventSO loadEventSO;
+    public GameSceneSO firstLoadScene;
+
+    [SerializeReference] private GameSceneSO _currentLoadScene;
+    private GameSceneSO _sceneToGo;
+    private Vector3 _positionToGo;
+    private bool _fadeScene;
+    public float fadeDuration;
+
+
+    private void Awake()
     {
-        
+        _currentLoadScene = firstLoadScene;
+        firstLoadScene.sceneReference.LoadSceneAsync(LoadSceneMode.Additive);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        loadEventSO.loadRequestEvent -= OnLoadEventSO;
+    }
+
+
+    private void OnEnable()
+    {
+        loadEventSO.loadRequestEvent += OnLoadEventSO;
+    }
+
+
+    private void OnLoadEventSO(GameSceneSO scene, Vector3 posToGo, bool fadeScene)
+    {
+        _sceneToGo = scene;
+        _positionToGo = posToGo;
+        _fadeScene = fadeScene;
+        Debug.Log($"OnLoadEventSO: {_sceneToGo.sceneReference.SubObjectName}");
+        StartCoroutine(UnloadPreviousScene());
+    }
+
+    private IEnumerator UnloadPreviousScene()
+    {
+        if (_fadeScene)
+        {
+        }
+
+        yield return new WaitForSeconds(fadeDuration);
+        if (_currentLoadScene != null)
+        {
+            yield return _currentLoadScene.sceneReference.UnLoadScene();
+        }
+
+        LoadNewScene();
+    }
+
+    private void LoadNewScene()
+    {
+        _sceneToGo.sceneReference.LoadSceneAsync(LoadSceneMode.Additive, true);
     }
 }
