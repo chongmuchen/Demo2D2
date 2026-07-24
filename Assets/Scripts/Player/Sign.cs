e42c70373eba2be8ff291e2c9e3abae7eeefa049
chongmuchen <chongmu.chen@gmail.com>
2026-07-24 12:17:37 +0800

    hole

--- Assets/Scripts/Player/Sign.cs
+++ Assets/Scripts/Player/Sign.cs
@@ -1,16 +1,60 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.DualShock;
 
 public class Sign : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerInputControl _playerInputControl;
+    private Animator anim;
+    public GameObject spireSign;
+    public Transform playerTrans;
+    private bool canPress;
+
+    private void Awake()
     {
-        
+        anim = spireSign.GetComponent<Animator>();
+        _playerInputControl = new PlayerInputControl();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        InputSystem.onActionChange += OnActionChange;
+    }
+
+    private void OnActionChange(object obj, InputActionChange actionChange)
+    {
+        if (actionChange == InputActionChange.ActionStarted)
+        {
+            var d = ((InputAction)obj).activeControl.device;
+            switch (d.device)
+            {
+                case Keyboard:
+                    anim.Play("Keyboard");
+                    break;
+                case DualShockGamepad:
+                    anim.Play("ps");
+                    break;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        spireSign.GetComponent<SpriteRenderer>().enabled = canPress;
+        spireSign.transform.localScale = playerTrans.localScale;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            canPress = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        canPress = false;
     }
 }
