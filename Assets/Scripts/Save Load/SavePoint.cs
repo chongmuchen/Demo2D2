c503daa67e1210c98d980fb5845c868aabb6e323
chongmuchen <chongmu.chen@gmail.com>
2026-07-25 15:01:44 +0800

    切场景

---
+++ Assets/Scripts/Save Load/SavePoint.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class SavePoint : MonoBehaviour, IIteractable
+{
+    public SpriteRenderer _spriteRenderer;
+    public Sprite darkSprite;
+    public Sprite lightSprite;
+    public bool isDone;
+
+    public void Awake()
+    {
+        // _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        _spriteRenderer.sprite = isDone ? lightSprite : darkSprite;
+    }
+
+    public void TriggerAction()
+    {
+        if (!isDone)
+        {
+            isDone = true;
+            _spriteRenderer.sprite = lightSprite;
+            this.gameObject.tag = "Untagged";
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject newGameButton;
+
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(newGameButton);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game");
+        Application.Quit();
+    }
+} 
